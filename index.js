0d2ae69288dbe9261169496b419ce6a7901c5b29
yasirmaxstyle <yasirmu77@gmail.com>
2025-04-23 15:20:33 +0700

    edit condition input

--- index.js
+++ index.js
@@ -10,20 +10,22 @@ const dateConvert = async () => {
   // validate using regex format
   const formatDate = /\d{2}-\d{2}-\d{4}/;
   if (output === "Invalid date") {
-    rl.close()
-    throw new Error('Invalid date')
-  } else if (dateInput.match(formatDate)) {
+    rl.close();
+    throw new Error('Invalid date');
+  } else if (!dateInput.match(formatDate)) {
+    rl.close();
+    throw new Error('Invalid format');
+  }
+  else {
     rl.close();
     return output;
-  }
-
-  else throw new Error('Invalid format');
-}
+  };
+};
 
 try {
-  const res = await dateConvert()
-  console.log(res)
+  const res = await dateConvert();
+  console.log(res);
 } catch (error) {
-  if (error.message === "Invalid date") console.log('Tanggal tidak valid.')
-  if (error.message === 'Invalid format') console.log('Format tanggal salah.')
+  if (error.message === "Invalid date") console.log('Tanggal tidak valid.');
+  if (error.message === 'Invalid format') console.log('Format tanggal salah.');
 }

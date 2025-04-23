62f9b183104cedcc0d38a84cbe8896d5dbc3e9a1
yasirmaxstyle <yasirmu77@gmail.com>
2025-04-23 13:02:34 +0700

    add program to convert format date

---
+++ index.js
@@ -0,0 +1,19 @@
+import moment from "moment"
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+
+const rl = readline.createInterface({ input, output });
+
+const dateConvert = async () => {
+    const dateInput = await rl.question('Masukkan tanggal dengan format (DD/MM/YYYY): ')
+    const output = moment(dateInput, "DD-MM-YYYY").format("DD/MM/YYYY")
+    const formatDate = /\d{2}\-\d{2}\-\d{4}/
+    if (dateInput.match(formatDate)) {
+        console.log(output)
+        rl.close()
+    }
+    else console.log('Format tanggal salah.')
+
+}
+
+dateConvert()
